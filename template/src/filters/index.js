0d968d985f50e8d9095ce197d659fd3a6b46707f
helloLaoYang <aaron@codonas.cn>
2017-11-14 20:26:16 +0800

    debug: 1. 处理全局过滤器失效问题

--- template/src/filters/index.js
+++ template/src/filters/index.js
@@ -3,16 +3,16 @@ import { resourceURL, env } from '../config'{{#if_eq lintConfig "airbnb"}};{{/if
 
 export default class {
   // http协议转http
-  static http2https{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}(URL) {
+  http2https{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}(URL) {
     return URL.replace('http', 'https'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  }
+  },
   // 补全资源链接
   // 当资源链接无协议类型时，进行链接补全
-  static replenishURL{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}(URL) {
+  replenishURL{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}(URL) {
     // has http?
     if (URL.indexOf('http') >= 0) {
       return URL{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
     }
     return `${resourceURL[env]}${URL}`{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-  }
+  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }
